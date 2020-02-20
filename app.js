fb3bc089c542272b23aa89cfb576a31f00a84fbd
muna <muna.jebril1995@hotmail.com>
2020-02-20 14:25:44 +0200

    sumArray

--- app.js
+++ app.js
@@ -87,12 +87,19 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+    var sumA=sumAndMultiply(sumArr[0],sumArr[1],sumArr[2]);
+var y=sumA[0];
+
+//  console.log(y);
+//  var letter1='2,3,4 was passed in as an array of numbers, and 9 is their sum.'
+ var letter1=`${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${y} is their sum.`;
+return[y,letter1]; 
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
